9c069aca4467aeda5506865f863c461289c5903d
a99lex <alex.schou@gmail.com>
2026-09-23 10:12:55 +0200

    Build DBAForge v0.1

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"DBAForge",description:"Oracle & PostgreSQL DBA command center"};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
